73e78e5bb461d6d0938f76824b63a2f0019eaaea
Frederick Morin <freddydumont@users.noreply.github.com>
2023-11-15 00:19:35 +0700

    Format code

--- game.gd
+++ game.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
+
 func _draw():
 	## vertical padding. looks better when line doesn't start at the edge
-	const offset = 4
+	const OFFSET = 4
 
 	var line_x = get_viewport_rect().size.x / 2
-	draw_dashed_line(Vector2(line_x, offset), Vector2(line_x, get_viewport_rect().size.y - offset), Color.WHITE, 2, 4)
+	draw_dashed_line(
+		Vector2(line_x, OFFSET),
+		Vector2(line_x, get_viewport_rect().size.y - OFFSET),
+		Color.WHITE,
+		2,
+		4
+	)
 
 
 # Called when the node enters the scene tree for the first time.
