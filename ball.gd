4925bc30613dbaffe51114e374a5df9674346b73
Frederick Morin <freddydumont@users.noreply.github.com>
2023-11-18 02:24:27 +0700

    Increase speed when colliding with paddle

--- ball.gd
+++ ball.gd
@@ -2,7 +2,8 @@ class_name Ball
 extends CharacterBody2D
 
 @export var color = Color.WHITE
-@export var initial_speed = 200
+@export var speed = 240
+@export var speed_increase = 60
 ## The maximum angle variation from the horizontal axis (in degrees).
 ## Goes in both y and -y so total sector is var * 2
 @export var launch_angle_range = 70
@@ -27,7 +28,7 @@ func launch_ball():
 		launch_angle += PI  # This will send the ball to the right
 
 	# Set the velocity according to the launch angle and speed
-	velocity = Vector2(cos(launch_angle), sin(launch_angle)) * initial_speed
+	velocity = Vector2(cos(launch_angle), sin(launch_angle)).normalized() * speed
 
 
 func _ready():
@@ -38,3 +39,13 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.get_normal())
+
+		# when ball collides with paddle, increase its speed
+		if collision.get_collider() is Paddle:
+			speed += speed_increase
+			velocity = velocity.normalized() * speed
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	# Deletes the ball when it exits the screen
+	queue_free()
