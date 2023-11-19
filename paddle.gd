0082e09f93e1e6dc4f93f5bdd2b6458bf34bfc69
Frederick Morin <freddydumont@users.noreply.github.com>
2023-11-20 00:29:05 +0700

    Reduce paddle collider width to a line

--- paddle.gd
+++ paddle.gd
@@ -4,22 +4,23 @@ extends StaticBody2D
 @export var speed = 12
 @export var is_player: bool = false
 @export var padding_x = 16
-
-var screen_size
-var paddle_offset
+## Actual size of the paddle drawn
+@export var size: Vector2 = Vector2(8, 32)
 
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	var screen_size = get_viewport_rect().size
 
 	# place the paddles on the screen
 	if is_player:
 		position = Vector2(padding_x, screen_size.y / 2)
 	else:
 		position = Vector2(screen_size.x - padding_x, screen_size.y / 2)
+		# collider isn't the same shape as the paddle, it's just the front
+		# it's drawn in the UI for the player, and flipped here for the enemy
+		$CollisionShape2D.position = -$CollisionShape2D.position
 
 
-# Gets the collision shape and draw a rectangle of the same dimensions
 func _draw():
-	var rect = $CollisionShape2D.shape.get_rect()
-	draw_rect(rect, Color.WHITE)
+	# first arg places the origin on the top left corner
+	draw_rect(Rect2(-(size / 2), size), Color.WHITE)
