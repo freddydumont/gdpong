726276910ceb180d86196799105529a3909c4a71
Frederick Morin <freddydumont@users.noreply.github.com>
2023-11-20 16:22:27 +0700

    Refactor ball bounce to depend on where it hits paddle

--- ball.gd
+++ ball.gd
@@ -5,14 +5,14 @@ const RoundWinner = preload("res://common.gd").RoundWinner
 
 signal round_won(winner: RoundWinner)
 
-@export var color = Color.WHITE
-@export var initial_speed = 240
-@export var speed_increase = 60
+@export var color := Color.WHITE
+@export var initial_speed := 240
+@export var speed_increase := 60
 ## The maximum angle variation from the horizontal axis (in degrees).
 ## Goes in both y and -y so total sector is var * 2
-@export var launch_angle_range = 60
+@export var launch_angle_range := 60
 
-var speed = initial_speed
+var speed := initial_speed
 
 enum LaunchSide {
 	LEFT,
@@ -67,24 +67,35 @@ func _on_timer_timeout():
 
 
 func _physics_process(delta):
-	var collision = move_and_collide(velocity * delta)
-	if collision:
-		velocity = velocity.bounce(collision.get_normal())
+	var collision := move_and_collide(velocity * delta)
 
-		# TODO: update paddle collision logic so that the bounce
-		# 			depends on where it hits on the paddle
-		if collision.get_collider() is Paddle:
+	if collision:
+		var collider := collision.get_collider()
+		if collider is Paddle:
 			# when ball collides with paddle, increase its speed
 			speed += speed_increase
 
-			velocity = velocity.normalized()
+			# Calculate the new direction based on where the ball hit the paddle
+			var hit_pos := global_position.y - (collider as Paddle).global_position.y
 
-			# Limit the angle of the bounce to a certain range so it doesn't go straight up
-			if abs(velocity.x) < 0.75:
-				velocity.x = 0.75 if velocity.x > 0 else -0.75
-				velocity.y = sign(velocity.y) * sqrt(1 - velocity.x ** 2)
+			var max_bounce_angle := PI / 4  # 45 degrees
+			var bounce_angle := hit_pos / ((collider as Paddle).size.y / 2) * max_bounce_angle
 
-			velocity = velocity * speed
+			# Calculate the new velocity vector
+			velocity.x = -sign(velocity.x) * speed * cos(bounce_angle)
+			velocity.y = speed * sin(bounce_angle)
+		else:
+			# For collisions with top and bottom walls
+			velocity = velocity.bounce(collision.get_normal())
+
+		velocity = velocity.normalized()
+
+		# Limit the angle of the bounce to a certain range so it doesn't go straight up
+		if abs(velocity.x) < 0.75:
+			velocity.x = 0.75 if velocity.x > 0 else -0.75
+			velocity.y = sign(velocity.y) * sqrt(1 - velocity.x ** 2)
+
+		velocity *= speed
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
